8ed46e0bd530d1c8bba2ed1d2ce8e442fe42742c
choir27 <richardchoi54@gmail.com>
2022-06-28 15:25:05 -0400

    added subject select for form

--- assets/js/main.js
+++ assets/js/main.js
@@ -273,3 +273,19 @@ function hideButton(){
 
 }
 
+
+
+function changeFormName(event) {
+	// The value of the subject field.
+	let value = event.target.value;
+	// The name we want to apply to the form, based on the value.
+	let name = "Contact-" + value;
+	// The form element in the DOM.
+	let form = document.querySelector("#conditional-form");
+	// Apply the new name to the form's [name] attribute.
+	if (form) form.setAttribute("name", name);
+	// The [name="form-name] field in the DOM.
+	let formName = document.querySelector('#conditional-form [name="form-name"]');
+	// Apply the new name to the [name="form-name"] field within the form.
+	if (formName) formName.setAttribute("value", name);
+  }
